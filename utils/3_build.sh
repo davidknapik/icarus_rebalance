947246d8b2babd3a8c975042ead9cda30d1a309b
David Knapik <david.knapik@gmail.com>
2023-07-27 08:40:00 -0700

    standardizing directory naming scheme

--- utils/3_build.sh
+++ utils/3_build.sh
@@ -1,22 +1,33 @@
 #!/bin/sh
 
+BASEDIR='/c/Temp/icarus'
+
 # clean up
-cd /c/Temp/icarus
-rm -rf staging
-rm mymod.pak
+echo -e "\n# Cleaning up old build"
+cd $BASEDIR
+rm -rf icarus_staging
+rm icarus_mod.pak
 rm modfiles.txt
 
+
 # create modfiles.txt
-cd /c/Temp/icarus/icarus_rebalance 
-git diff --name-only master..rocket data/ > /c/Temp/icarus/modfiles.txt
+echo -e "\n# Creating modfile.txt"
+cd $BASEDIR/icarus_mod/icarus_rebalance 
+git diff --name-only mod..rocket data/ > $BASEDIR/modfiles.txt
 
 # build staging area
-cd /c/Temp/icarus
-mkdir staging
-rsync -avz --files-from=modfiles.txt icarus_rebalance staging/.
+echo -e "\n# Create & populate staging area"
+cd $BASEDIR
+mkdir icarus_staging
+rsync -avz --files-from=modfiles.txt icarus_mod/icarus_rebalance icarus_staging/.
+
+# validate json files
+echo -e "\n# Validate JSON files"
+find icarus_staging -type f -name '*.json' -exec python icarus_utils/icarus_rebalance/utils/validate_json.py {} \;
 
 # build mod file
-cd staging/data
-/c/Temp/icarus/repak/repak.exe pack --version V11 --mount-point ../../../Icarus/Content/data/ . /c/Temp/icarus/mymod.pak
+echo -e "\n# Create icarus_mod.pak"
+cd icarus_staging/data
+$BASEDIR/repak/repak.exe pack --version V11 --mount-point ../../../Icarus/Content/data/ . $BASEDIR/icarus_mod.pak
 
 
